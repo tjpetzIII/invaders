4fc585f604286fa2f8f58cd37456716ceaf9d90f
Thomas Petz <tompetz2000@gmail.com>
2024-03-16 13:40:27 -0400

    Removed the sound and started the game loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, io, time::Duration};
+
+use crossterm::{cursor::{Hide, Show}, event::{self, Event, KeyCode}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand};
+
+fn main() -> Result <(), Box<dyn Error>> {
+    //terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    //Game Loop
+    'gameloop: loop {
+        //input
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                  KeyCode::Esc | KeyCode::Char('q') => {
+                    break 'gameloop;
+                  }
+                  _ => {}
+                }
+            }
+        }
+
+    }
+
+    //cleanup
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    Ok(())
+
+
 }
