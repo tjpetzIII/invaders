96e2ab9873dffa3541ff9c4bb09cfba8fa1c9d25
Thomas Petz <tompetz2000@gmail.com>
2024-03-16 14:18:32 -0400

    Added rendering to the game

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::{error::Error, io, time::Duration};
+use std::{error::Error, io, sync::mpsc, thread, time::Duration};
 
 use crossterm::{cursor::{Hide, Show}, event::{self, Event, KeyCode}, terminal::{self, EnterAlternateScreen, LeaveAlternateScreen}, ExecutableCommand};
+use invaders::{frame::{self, new_frame}, render::{self, render}};
 
 fn main() -> Result <(), Box<dyn Error>> {
     //terminal
@@ -9,8 +10,27 @@ fn main() -> Result <(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
+    // Render loop in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn( move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
     //Game Loop
     'gameloop: loop {
+        // per-frame init
+        let curr_frame = new_frame();
+
         //input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
@@ -23,9 +43,15 @@ fn main() -> Result <(), Box<dyn Error>> {
             }
         }
 
+        // draw and render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+
     }
 
     //cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
     terminal::disable_raw_mode()?;
